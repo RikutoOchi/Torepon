298dd10d46d281d66a6629f73ea3ac1ba4c60ce8
tomigaki <tomigaki@example.com>
2022-06-30 14:31:22 +0900

    データベース接続のためのファイルの変更

--- loginpdo.php
+++ loginpdo.php
@@ -5,5 +5,9 @@
 	$password = 'contact';
 	$pdo = new PDO($dsn, $user, $password);
 
+	// 画面表示データのエスケープ処理用関数
+	function h ( $data ) {
+		return htmlspecialchars($data, ENT_QUOTES, "UTF-8");
+	}
 # ユーザー「login」にパスワード「contact」を設定し、データベース「torepon」に対する全ての権限を付与
 # grant all privileges on torepon.* to login@localhost identified by 'contact';

e1748987cd0cae467f01e630535312475a3b451c
nakagoe <kd1307490@st.kobedenshi.ac.jp>
2022-07-07 11:54:31 +0900

    出品リスト表示崩れ

--- ex-list.php
+++ ex-list.php
@@ -17,27 +17,23 @@
     <!-- mainコンテンツ -->
     <h1>出品リスト</h1>
      <div class="item-list"> 
-      <div class="responsive">
+        <div class="responsive">
           <div class="img">
             <a target="_blank" href="./ex-confirm.html">
               <img
                 src=".\images\keyholder_kuma.png"
                 alt="くま"
-                width="300"
-                height="300"
               />
             </a>
             <div class="desc">動物キーホルダー くま</div>
           </div>
-      </div>
+        </div>
         <div class="responsive">
           <div class="img">
             <a target="_blank" href="./ex-confirm.html">
               <img
                 src="https://bandai-a.akamaihd.net/bc/img/model/b/1000168623_2.jpg"
                 alt="アイテム名"
-                width="300"
-                height="300"
               />
             </a>
             <div class="desc">アイテム名</div>
@@ -49,8 +45,6 @@
               <img
                 src="https://bandai-a.akamaihd.net/bc/img/model/b/1000170272_2.jpg"
                 alt="アイテム名"
-                width="300"
-                height="300"
               />
             </a>
             <div class="desc">アイテム名</div>
@@ -62,8 +56,6 @@
               <img
                 src="https://bandai-a.akamaihd.net/bc/img/model/b/1000171571_2.jpg"
                 alt="アイテム名"
-                width="300"
-                height="300"
               />
             </a>
             <div class="desc">アイテム名</div>
@@ -75,8 +67,6 @@
               <img
                 src="https://bandai-a.akamaihd.net/bc/img/model/b/1000172915_2.jpg"
                 alt="アイテム名"
-                width="300"
-                height="300"
               />
             </a>
             <div class="desc">アイテム名</div>
@@ -88,8 +78,6 @@
               <img
                 src="https://bandai-a.akamaihd.net/bc/img/model/b/1000172905_2.jpg"
                 alt="アイテム名"
-                width="300"
-                height="300"
               />
             </a>
             <div class="desc">アイテム名</div>
@@ -98,20 +86,20 @@
         <div class="responsive">
           <div class="img">
             <a target="_blank" href="./ex-confirm.html">
-              <img src="" alt="アイテム名" width="300" height="300" />
+              <img src="" alt="アイテム名"/>
             </a>
             <div class="desc">アイテム名</div>
           </div>
         </div>
-          <div class="responsive">
+        <div class="responsive">
             <div class="img">
               <a target="_blank" href="./ex-confirm.html">
-                <img src="" alt="アイテム名" width="300" height="300" />
+                <img src="" alt="アイテム名"/>
               </a>
               <div class="desc">アイテム名</div>
             </div>
-          </div>
-   </div>
+        </div>
+      </div>
   </section>
 
 

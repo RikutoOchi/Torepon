ebf7f0212aa706b16404b3ce93223e98b6f14f70
tomigaki <tomigaki@example.com>
2022-09-09 09:57:12 +0900

    個人保存

---
+++ dbdata.php
@@ -0,0 +1,42 @@
+<?php                     
+  class  DbData {              // DbDataクラスの宣言     
+                      
+      protected  $pdo;         // PDOオブジェクト用のプロパティ（メンバ変数）の宣言     
+                      
+      public function __construct( ) {   // コンストラクタ     
+          // PDOオブジェクトを生成する                     
+          $dsn = 'mysql:host=localhost;dbname=torepon;charset=utf8';                     
+          $user = 'shopping';                     
+          $password = 'site';                     
+          try{                      
+              $this->pdo = new PDO($dsn, $user, $password);                     
+          } catch(Exception  $e){                     
+              echo 'Error:' . $e->getMessage( );              
+              die( );                     
+          }                     
+      }                     
+                      
+      protected function query ( $sql,  $array_params ) {  
+          $stmt = $this->pdo->prepare( $sql );                      
+          $stmt->execute( $array_params );                      
+          return  $stmt;          
+      } 
+      public function getRecord($table,$column,$id){
+        $sql = "select * from $table where $column = ?";
+        $stmt = $this->query($sql, [$id]);
+        $record = $stmt->fetch();
+        return $record;
+    }
+      public  function  getRecords ($table,$column,$value) {
+        $sql = "select * from $table where $column = ?";
+        $stmt = $this->query($sql, [$value]);
+        $records = $stmt->fetchAll( );
+        return  $records;
+    }
+                          
+                      
+      protected function exec ( $sql,  $array_params ) {  
+          $stmt = $this->pdo->prepare( $sql );                      
+          return  $stmt->execute( $array_params ); 
+      }                     
+  }                     

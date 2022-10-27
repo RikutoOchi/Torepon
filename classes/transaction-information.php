df7bcdd9d7af5c32550c87c0eab47d6714c8653f
Naoto000 <kd1289594@st.kobedenshi.ac.jp>
2022-10-28 08:39:10 +0900

    a

--- classes/transaction-information.php
+++ classes/transaction-information.php
@@ -3,10 +3,12 @@
 
   class Transaction_information_data extends DbData{
      public function getRecord_transaction_information_data($table1,$table2,$table3,$culum1,$culum2,$culum3,$value) {
-        $sql = "select * from (($table1 left outer join $table2 on $table1.$culum1 = $table2.$culum2) left outer join $table3 on $table2.$culum3 = $table3.$culum3)
+        $sql = "select * from (($table1 left outer join $table2 on $table1.$culum1 = $table2.$culum2) left outer join $table3 on $table1.$culum3 = $table3.$culum3)
                 where $table1.$culum1 = ?";
         $stmt = $this->query($sql, [$value]);
         $records = $stmt->fetchAll( );
         return  $records;
     }
   }
+
+  //('exhibits','trades','users','EXHIBIT_ID','TRADE_ID','USER_ID',$id);

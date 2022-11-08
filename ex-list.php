1796046951088dd6e830dd477c120ce1c63949e8
Naoto000 <kd1289594@st.kobedenshi.ac.jp>
2022-11-08 09:38:11 +0900

    並び替えボタンを押下した時に発生するエラーの解消

--- ex-list.php
+++ ex-list.php
@@ -22,7 +22,7 @@
       $sarch_id = $_GET['sarch_id'];
     /* ------------------------------------------ */
 
-
+    // 絞り込み条件が設定された場合
     if($sarch_id != 0){
 
         if($_SESSION["gatya"] == ""){
@@ -65,9 +65,9 @@
           $maisu_end = "9999999";
         } else{
           $maisu_end = $_SESSION['maisu-end']; 
-        };
-      /* ------------------------------------------------------------------------------------- */
-    } else{  
+        }
+
+    } else{
       $gatya = "%_%";
       $kyara = "%_%";
       $gensaku = "%_%";
@@ -83,10 +83,7 @@
       $_SESSION["nitizi-start"] = $nitizi_start;
       $_SESSION["syurui"] = $syurui;
       $_SESSION["maisu-end"] = $maisu_end;
-
-
     }
-
 
 
     /* ----------------------------------- db接続関連 --------------------------------------- */
@@ -94,6 +91,7 @@
       $exh = new Dbdata();
     /* -------------------------------------------------------------------------------------- */
 
+      // 新しい順（投稿日時が新しい順）
       if ($sort_id == 0) {
         $sql = "select EXHIBITS.EXHIBIT_PIC_URL,EXHIBITS.EXHIBIT_ID,EXHIBITS.EXHIBIT_NAME 
         from (((EXHIBITS LEFT OUTER JOIN GACHA_TITLES ON EXHIBITS.GACHA_TITLE_ID = GACHA_TITLES.GACHA_TITLE_ID) 
@@ -112,12 +110,12 @@
         from (((EXHIBITS LEFT OUTER JOIN GACHA_TITLES ON EXHIBITS.GACHA_TITLE_ID = GACHA_TITLES.GACHA_TITLE_ID) 
         LEFT OUTER JOIN ORIGINAL_TITLES ON GACHA_TITLES.ORIGINAL_TITLE_ID = ORIGINAL_TITLES.ORIGINAL_TITLE_ID)
         LEFT OUTER JOIN MAKERS ON GACHA_TITLES.MAKER_ID = MAKERS.MAKER_ID)
-        where GACHA_TITLES.GACHA_TITLES LIKE '" . $gatya . "' 
-        EXHIBITS.EXHIBIT_TEXT LIKE '" . $kyara . "' 
-        ORIGINAL_TITLES.ORIGINAL_TITLE_NAME LIKE '" . $gensaku . "' 
-        MAKERS.MAKER_NAME LIKE '" . $meka . "' 
-        EXHIBITS.EXHIBIT_TIME >= '" . $nitizi_start . "' 
-        EXHIBITS.TICKET_TYPE_ID LIKE '" . $syurui . "' 
+        where GACHA_TITLES.GACHA_TITLE_NAME LIKE '" . $gatya . "' and 
+        EXHIBITS.EXHIBIT_TEXT LIKE '" . $kyara . "' and 
+        ORIGINAL_TITLES.ORIGINAL_TITLE_NAME LIKE '" . $gensaku . "' and 
+        MAKERS.MAKER_NAME LIKE '" . $meka . "' and
+        EXHIBITS.EXHIBIT_TIME >= '" . $nitizi_start . "' and 
+        EXHIBITS.TICKET_TYPE_ID LIKE '" . $syurui . "' and
         EXHIBITS.NUMBER_OF_TICKETS <= '" . $maisu_end . "' order by EXHIBIT_TIME desc";
       // 必要チケット枚数　昇順（多い順）
       } elseif($sort_id == 2) {
@@ -125,12 +123,12 @@
         from (((EXHIBITS LEFT OUTER JOIN GACHA_TITLES ON EXHIBITS.GACHA_TITLE_ID = GACHA_TITLES.GACHA_TITLE_ID) 
         LEFT OUTER JOIN ORIGINAL_TITLES ON GACHA_TITLES.ORIGINAL_TITLE_ID = ORIGINAL_TITLES.ORIGINAL_TITLE_ID)
         LEFT OUTER JOIN MAKERS ON GACHA_TITLES.MAKER_ID = MAKERS.MAKER_ID)
-        where GACHA_TITLES.GACHA_TITLES LIKE '" . $gatya . "' 
-        EXHIBITS.EXHIBIT_TEXT LIKE '" . $kyara . "' 
-        ORIGINAL_TITLES.ORIGINAL_TITLE_NAME LIKE '" . $gensaku . "' 
-        MAKERS.MAKER_NAME LIKE '" . $meka . "' 
-        EXHIBITS.EXHIBIT_TIME >= '" . $nitizi_start . "' 
-        EXHIBITS.TICKET_TYPE_ID LIKE '" . $syurui . "' 
+        where GACHA_TITLES.GACHA_TITLE_NAME LIKE '" . $gatya . "' and
+        EXHIBITS.EXHIBIT_TEXT LIKE '" . $kyara . "' and
+        ORIGINAL_TITLES.ORIGINAL_TITLE_NAME LIKE '" . $gensaku . "' and
+        MAKERS.MAKER_NAME LIKE '" . $meka . "' and
+        EXHIBITS.EXHIBIT_TIME >= '" . $nitizi_start . "' and
+        EXHIBITS.TICKET_TYPE_ID LIKE '" . $syurui . "' and
         EXHIBITS.NUMBER_OF_TICKETS <= '" . $maisu_end . "' order by NUMBER_OF_TICKETS";
       // 必要チケット枚数　降順（少ない順）
       } elseif($sort_id == 3) {
@@ -138,12 +136,12 @@
         from (((EXHIBITS LEFT OUTER JOIN GACHA_TITLES ON EXHIBITS.GACHA_TITLE_ID = GACHA_TITLES.GACHA_TITLE_ID) 
         LEFT OUTER JOIN ORIGINAL_TITLES ON GACHA_TITLES.ORIGINAL_TITLE_ID = ORIGINAL_TITLES.ORIGINAL_TITLE_ID)
         LEFT OUTER JOIN MAKERS ON GACHA_TITLES.MAKER_ID = MAKERS.MAKER_ID)
-        where GACHA_TITLES.GACHA_TITLES LIKE '" . $gatya . "' 
-        EXHIBITS.EXHIBIT_TEXT LIKE '" . $kyara . "' 
-        ORIGINAL_TITLES.ORIGINAL_TITLE_NAME LIKE '" . $gensaku . "' 
-        MAKERS.MAKER_NAME LIKE '" . $meka . "' 
-        EXHIBITS.EXHIBIT_TIME >= '" . $nitizi_start . "' 
-        EXHIBITS.TICKET_TYPE_ID LIKE '" . $syurui . "' 
+        where GACHA_TITLES.GACHA_TITLE_NAME LIKE '" . $gatya . "' and
+        EXHIBITS.EXHIBIT_TEXT LIKE '" . $kyara . "' and
+        ORIGINAL_TITLES.ORIGINAL_TITLE_NAME LIKE '" . $gensaku . "' and
+        MAKERS.MAKER_NAME LIKE '" . $meka . "' and
+        EXHIBITS.EXHIBIT_TIME >= '" . $nitizi_start . "' and
+        EXHIBITS.TICKET_TYPE_ID LIKE '" . $syurui . "' and
         EXHIBITS.NUMBER_OF_TICKETS <= '" . $maisu_end . "' order by NUMBER_OF_TICKETS desc";
       }
 
@@ -153,19 +151,24 @@
   <!-- mainコンテンツ -->
     <h1>出品リスト</h1><br>
 
-    <font size="4">絞り込み条件</font><br>
+    <div class="search-box"><div class="searchBy-ttl">絞り込み条件</div><button class="disp-search"></button></div>
       <font size="2">
 
 <!------------- セッション（$_SESSION）にデータを格納させるために、data retension.phpへデータを飛ばす ------------>
       <form action="./dataretention2.php?id=0" method="post" name="terms_form">
-        　ガチャタイトル　：　　<input type="text" name="gatya"><br>
-        　キャラクター名　：　　<input type="text" name="kyara"><br>
-        　原　　　　　作　：　　<input type="text" name="gensaku"><br>
-        　メ　ー　カ　ー　：　　<input type="text" name="me-ka-"><br>
-        　出　品　日　時　：　　<input type="date" name="nitizi-start">～<br>
-        　チケット種類　　：　　<input type="text" name="syurui"><br>
-        　チケット枚数　　：　～<input type="text" name="maisu-end">　
-        <input type="button" value="絞り込む" class="search" onclick="document.terms_form.submit();"/><br><br>
+        <ul class="searchBy-list none">
+          <li>ガチャタイトル　:　<input class="searchBy-text" type="text" name="gatya" ></li>
+          <li>キャラクター名　:　<input class="searchBy-text" type="text" name="kyara" ></li>
+          <li>原　　　　　作　:　<input class="searchBy-text" type="text" name="gensaku" ></li>
+          <li>メ　ー　カ　ー　:　<input class="searchBy-text" type="text" name="me-ka-"></li>
+          <li>出　品　日　時　:　<input class="searchBy-text mr-none" type="date" name="nitizi-start">～</li>
+          <li>チケット種類　　:　<input class="searchBy-text" type="text" name="syurui"></li>
+          <li>チケット枚数　　:　～<input class="searchBy-text ml-none" type="text" name="maisu-end">
+          <input type="button" value="絞り込む" class="search" onclick="document.terms_form.submit();"/>
+        </li>
+
+        </ul>
+        
       </form>
 <!------------------------------------------------------------------------------------------------------------->
 
@@ -174,31 +177,30 @@
     <br><label>並び順</label><br>
 
     <?php if($sort_id == 0){ ?>
-      <input type="button" value="新しい順" class="sort-btn" style="background-color:#87CEFA" onclick="location.href='ex-list.php?sort_id=0'"/>
-      <input type="button" value="古い順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=1'"/>
+      <input type="button" value="新しい順" class="sort-btn" style="background-color:#87CEFA" onclick="location.href='ex-list.php?sort_id=0&sarch_id=1'"/>
+      <input type="button" value="古い順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=1&sarch_id=1'"/>
       <label>　　</label>
-      <input type="button" value="チケット数　昇順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=2'"/>
-      <input type="button" value="チケット数　降順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=3'"/>
+      <input type="button" value="チケット数　昇順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=2&sarch_id=1'"/>
+      <input type="button" value="チケット数　降順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=3&sarch_id=1'"/>
     <?php } elseif($sort_id == 1){ ?>
-      <input type="button" value="新しい順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=0'"/>
-      <input type="button" value="古い順" class="sort-btn" style="background-color:#87CEFA" onclick="location.href='ex-list.php?sort_id=1'"/>
+      <input type="button" value="新しい順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=0&sarch_id=1'"/>
+      <input type="button" value="古い順" class="sort-btn" style="background-color:#87CEFA" onclick="location.href='ex-list.php?sort_id=1&sarch_id=1'"/>
       <label>　　</label>
-      <input type="button" value="チケット数　昇順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=2'"/>
-      <input type="button" value="チケット数　降順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=3'"/>
+      <input type="button" value="チケット数　昇順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=2&sarch_id=1'"/>
+      <input type="button" value="チケット数　降順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=3&sarch_id=1'"/>
     <?php } elseif($sort_id == 2){ ?>
-      <input type="button" value="新しい順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=0'"/>
-      <input type="button" value="古い順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=1'"/>
+      <input type="button" value="新しい順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=0&sarch_id=1'"/>
+      <input type="button" value="古い順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=1&sarch_id=1'"/>
       <label>　　</label>
-      <input type="button" value="チケット数　昇順" class="sort-btn" style="background-color:#87CEFA" onclick="location.href='ex-list.php?sort_id=2'"/>
-      <input type="button" value="チケット数　降順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=3'"/>
+      <input type="button" value="チケット数　昇順" class="sort-btn" style="background-color:#87CEFA" onclick="location.href='ex-list.php?sort_id=2&sarch_id=1'"/>
+      <input type="button" value="チケット数　降順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=3&sarch_id=1'"/>
     <?php } elseif($sort_id == 3){ ?>
-      <input type="button" value="新しい順" class="sort-btn" onclick="location.href='ex-listhp?sort_id=0'"/>
-      <input type="button" value="古い順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=1'"/>
+      <input type="button" value="新しい順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=0&sarch_id=1'"/>
+      <input type="button" value="古い順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=1&sarch_id=1'"/>
       <label>　　</label>
-      <input type="button" value="チケット数　昇順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=2'"/>
-      <input type="button" value="チケット数　降順" class="sort-btn" style="background-color:#87CEFA">
+      <input type="button" value="チケット数　昇順" class="sort-btn" onclick="location.href='ex-list.php?sort_id=2&sarch_id=1'"/>
+      <input type="button" value="チケット数　降順" class="sort-btn" style="background-color:#87CEFA" onclick="location.href='ex-list.php?sort_id=3&sarch_id=1'"/>
     <?php } ?>
-    <br><br>
 
     <div class="item-list"> 
         <?php foreach($data as $data_part){ ?>

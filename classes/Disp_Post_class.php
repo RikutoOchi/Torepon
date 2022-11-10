8f1e8c0c7335f4f516742dfffeb594c2190946e1
Naoto000 <kd1289594@st.kobedenshi.ac.jp>
2022-11-10 15:10:50 +0900

    コメント追加

--- classes/Disp_Post_class.php
+++ classes/Disp_Post_class.php
@@ -3,6 +3,7 @@
     // スーパークラスであるDbDataを利用するため
     require_once __DIR__ . '/dbdata.php';
 
+    // ガチャタイトルを取得
     class Disp_Post_class extends DbData{
         public function get_records_gacha_title () {
         $sql = "select distinct GACHA_TITLE_ID,GACHA_TITLE_NAME from gacha_titles";

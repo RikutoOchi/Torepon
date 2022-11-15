a73e0a9f2e2c72db9846542a3272dc85b12e7489
tomigaki <tomigaki@example.com>
2022-11-15 12:06:30 +0900

    修正してくれたチケット画面の追加

---
+++ classes/tradelist.php
@@ -0,0 +1,22 @@
+<?php
+  require_once __DIR__ . '/dbdata.php';
+
+  class Tradelist_exhibit extends DbData{
+     public function getRecord_tradelist_number_of_transactions ($value) {
+        $sql = "select * from exhibits left outer join trades on exhibits.EXHIBIT_ID = trades.TRADE_ID 
+                where trades.USER_ID = ? order by TRADE_START_TIME";
+        $stmt = $this->query($sql, [$value]);
+        $records = $stmt->fetchAll( );
+        return  $records;
+    }
+  }
+
+  class Tradelist_application extends DbData{
+    public  function  getRecords_order_by ($value) {
+      $sql = "select * from exhibits left outer join trades on exhibits.EXHIBIT_ID = trades.TRADE_ID 
+              where exhibits.USER_ID = ? order by TRADE_START_TIME";
+        $stmt = $this->query($sql, [$value]);
+        $records = $stmt->fetchAll( );
+        return  $records;
+    }
+  }

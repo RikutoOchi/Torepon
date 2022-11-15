05e089ec9d8502a26b389b60ee012a3d0a1e9fdc
tomigaki <tomigaki@example.com>
2022-11-15 12:08:25 +0900

    前にいじった部分

--- exlist-profile(理解).php
+++ exlist-profile(理解).php
@@ -12,6 +12,79 @@
 <?php require_once("./temp/header.php"); ?>
 <!-- /ヘッダー -->
 
+check.phpの理解
+<?php
+  require_once __DIR__ . './classes/dbdata.php';
+  // Dbdataオブジェクトを生成する
+  $exh = new Dbdata();
+  // ガチャタイトルを取り出す
+  $exhibit_gacha=$exh->getRecord('gacha_titles','GACHA_TITLE_ID',1);
+  // $exhibit_gacha = $exh->getRecords('gacha_titles','USER_ID',$exhibit['GACHA_TITLE_ID']);
+  // foreach($exhibit_gacha as $data_part){
+  //   $gc_name=$data_part['GACHA_TITLE_ID'];
+  // }
+  $now=new Dbdata();
+  //$exhibit_user = $now->getRecord('users','USER_ID',$exhibit['USER_ID']);
+  //$now('SELECT COUNT(EXHIBIT_ID) FROM exhibits WHERE USER_ID='1')
+  //$time=new Dbdata();
+  //$pdo->query('select * FROM exhibits') as $time
+?>
+<main class="main-side-content">
+  <section class="main-content">
+    <div>
+
+      <label>トレード履歴:</label>
+      <a href = "./ticket-confirm.php">詳細</a><br><br><br>
+      
+      <font size="5">チケット枚数</font>
+      <table border="1">
+        <tr bgcolor="#87cefa">
+          <th>チケット名</th>
+          <th>枚数</th>
+          <th>利用日時</th>
+        </tr>
+        <tr>
+          <th><?= $exhibit_gacha['GACHA_TITLE_NAME'] ?>用チケット</th>
+          <th>&nbsp;</th>
+          <th>htmlspecialchars($time['EXHIBIT_TIME'])</th>
+        </tr>
+        <tr>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </tr> 
+        <tr>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </tr> 
+        <tr>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </tr>
+        <tr>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </tr>
+        <tr>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </tr>
+        
+
+      </table>
+    </div>
+  </section>
+  <!-- サイドコンテンツ -->
+     <?php require_once('./temp/side.php'); ?>
+  <!-- /サイドコンテンツ -->
+
+</main>
+<!-- /mainコンテンツ -->
+ex-listの理解
 <?php
   $sort_id = $_GET['sort_id'];
   echo $sort_id;

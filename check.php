d86fab8f227c2e1ffa85225ed92d4b9171f8e84e
tomigaki <tomigaki@example.com>
2022-11-15 12:10:00 +0900

    confrimの愛用をcheckへ変更 自分が想定していた画面遷移は先に枚数確認だったため

--- check.php
+++ check.php
@@ -1,19 +1,3 @@
-<?php
-  require_once __DIR__ . './classes/dbdata.php';
-  // Dbdataオブジェクトを生成する
-  $exh = new Dbdata();
-  // ガチャタイトルを取り出す
-  $exhibit_gacha=$exh->getRecord('gacha_titles','GACHA_TITLE_ID',1);
-  // $exhibit_gacha = $exh->getRecords('gacha_titles','USER_ID',$exhibit['GACHA_TITLE_ID']);
-  // foreach($exhibit_gacha as $data_part){
-  //   $gc_name=$data_part['GACHA_TITLE_ID'];
-  // }
-  $now=new Dbdata();
-  //$exhibit_user = $now->getRecord('users','USER_ID',$exhibit['USER_ID']);
-  //$now('SELECT COUNT(EXHIBIT_ID) FROM exhibits WHERE USER_ID='1')
-  //$time=new Dbdata();
-  //$pdo->query('select * FROM exhibits') as $time
-?>
 <!-- ヘッドの全体に関わる共有部分 -->
 <?php require_once('./temp/head.php'); ?>
 <!-- /ヘッドの全体に関わる共有部分 -->
@@ -27,58 +11,58 @@
 <!-- ヘッダー -->
 <?php require_once("./temp/header.php"); ?>
 <!-- /ヘッダー -->
-<main class="main-side-content">
-  <section class="main-content">
-    <div>
 
-      <label>トレード履歴:</label>
+<?php
+
+  require_once __DIR__ . './classes/check.php';
+
+  // 自分が保持しているチケットの種類の一覧（チケットタイプID）を取得
+  $chicket_type = new Check_keep_chicket_type_data();
+  $chicket_type_data = $chicket_type->getRecord_check_keep_chicket_type_data($_SESSION['user_id']);
+
+?>
+
+<main class="main-side-content">
+      <section class="main-content">
+    </dl>
+
+    
+      <label>チケット履歴:</label>
       <a href = "./ticket-confirm.php">詳細</a><br><br><br>
       
-      <font size="5">チケット枚数</font>
+      <font size="5">チケット残数</font>
       <table border="1">
         <tr bgcolor="#87cefa">
           <th>チケット名</th>
           <th>枚数</th>
-          <th>利用日時</th>
         </tr>
-        <tr>
-          <th><?= $exhibit_gacha['GACHA_TITLE_NAME'] ?>用チケット</th>
-          <th>&nbsp;</th>
-          <th>htmlspecialchars($time['EXHIBIT_TIME'])</th>
-        </tr>
-        <tr>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-        </tr> 
-        <tr>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-        </tr> 
-        <tr>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-        </tr>
-        <tr>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-        </tr>
-        <tr>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-        </tr>
-        
 
+      <?php foreach($chicket_type_data as $chicket_type_data_detail) { ?>
+
+        <?php
+          require_once __DIR__ . './classes/check.php';
+          // 自分が保持しているチケットの名前とその枚数を取り出す
+          $chicket = new Check_keep_chicket_data();
+          $chicket_data = $chicket->getRecord_check_keep_chicket_data($_SESSION['user_id'],$chicket_type_data_detail['GACHA_TITLE_ID']);
+        ?>
+
+        <?php foreach($chicket_data as $chicket_data_detail){ ?>
+
+          <tr>
+            <th><?php echo $chicket_data_detail['GACHA_TITLE_NAME'] ?></th>
+            <th><?php echo $chicket_data_detail['NUMBER_OF_TICKETS'] ?></th>
+          </tr>
+
+        <?php } ?>
+
+      <?php } ?>
+      
       </table>
     </div>
   </section>
-  <!-- サイドコンテンツ -->
-     <?php require_once('./temp/side.php'); ?>
-  <!-- /サイドコンテンツ -->
+ <!-- サイドコンテンツ -->
+ <?php require_once('./temp/side.php'); ?>
+ <!-- /サイドコンテンツ -->
 
 </main>
 <!-- /mainコンテンツ -->

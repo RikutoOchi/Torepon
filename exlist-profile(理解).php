383ef76f65cf31df28c3db0c5528c0e8232d2131
tomigaki <tomigaki@example.com>
2022-11-25 13:58:59 +0900

    hekounasi

--- exlist-profile(理解).php
+++ exlist-profile(理解).php
@@ -175,3 +175,77 @@ ex-listの理解
   <!-- フッター -->
   <?php require_once('./temp/footer.php'); ?>
   <!-- /フッター -->
+
+classes/check.phpの理解
+<?php
+  require_once __DIR__ . '/dbdata.php';
+
+  // チケットの使用・入手履歴取得
+  class Check_chicket_data extends DbData{
+     public function getRecord_check_chicket_data ($value) {
+        //SQL文を定義する
+        $sql = "select trades.TRADE_FINISH_TIME,exhibits.NUMBER_OF_TICKETS,gacha_titles.GACHA_TITLE_NAME,tickets.USER_ID
+                from ((((( tickets left outer join ticket_types on tickets.TICKET_TYPE_ID = ticket_types.TICKET_TYPE_ID) 
+                left outer join ticket_trades on tickets.TICKET_ID = ticket_trades.TICKET_ID)
+                left outer join trades on ticket_trades.TRADE_ID = trades.TRADE_ID)
+                left outer join exhibits on trades.TRADE_ID = exhibits.EXHIBIT_ID)
+                left outer join gacha_titles on ticket_types.GACHA_TITLE_ID = gacha_titles.GACHA_TITLE_ID)
+                where tickets.USER_ID = ? or trades.USER_ID = ?
+                order by trades.TRADE_FINISH_TIME";
+        //実行する
+        $stmt = $this->query($sql, [$value,$value]);
+        //結果セットを取り出す
+        $records = $stmt->fetchAll( );
+        //結果セットを戻り値として返す
+        return  $records;
+    }
+  }
+
+  // 自分が持っているチケットの種類一覧を取得
+  class Check_keep_chicket_type_data extends DbData{
+    public function getRecord_check_keep_chicket_type_data ($value) {
+       $sql = "select gacha_titles.GACHA_TITLE_ID
+               from ((((( tickets left outer join ticket_types on tickets.TICKET_TYPE_ID = ticket_types.TICKET_TYPE_ID) 
+               left outer join ticket_trades on tickets.TICKET_ID = ticket_trades.TICKET_ID)
+               left outer join trades on ticket_trades.TRADE_ID = trades.TRADE_ID)
+               left outer join exhibits on trades.TRADE_ID = exhibits.EXHIBIT_ID)
+               left outer join gacha_titles on ticket_types.GACHA_TITLE_ID = gacha_titles.GACHA_TITLE_ID)
+               where tickets.USER_ID = ?";
+       $stmt = $this->query($sql, [$value]);
+       $records = $stmt->fetchAll( );
+       return  $records;
+   }
+ }
+  class Check_keep_chicket_data extends DbData{
+    public function getRecord_check_keep_chicket_data ($user_id,$value) {
+          //SQL文を定義する
+          $sql = "select gacha_titles.GACHA_TITLE_NAME //gacha_titlesテーブルのGACHA_TITLE_NAMEを指定
+                ,sum(exhibits.NUMBER_OF_TICKETS) as NUMBER_OF_TICKETS  //指定したGACHA_TITLE_NAMEの合計をチケット数として扱う。
+              from ((((
+                ①(tickets left outer join ticket_types on tickets.TICKET_TYPE_ID = ticket_types.TICKET_TYPE_ID) 
+                //ticketsテーブルにticket_typesテーブルを左外部結合する。
+                  その際、指定条件としてticketsテーブルのTICKET_TYPE_IDとticket_typesテーブルのTICKET_TYPE_IDが一致するものを指定する。
+                ②left outer join ticket_trades on tickets.TICKET_ID = ticket_trades.TICKET_ID)
+                //①のテーブルにticket_tradesテーブルを左外部結合する。
+                  その際、指定条件としてticketsテーブルのTICKET_IDとticket_tradesテーブルのTICKET_IDが一致する物を指定する。
+                ③left outer join trades on ticket_trades.TRADE_ID = trades.TRADE_ID)
+                //②のテーブルにtradesテーブルを左外部結合する。
+                  その際、指定条件としてticket_tradesテーブルのTRADE_IDとtradesテーブルのTRADE_IDが一致する物を指定する。
+                ④left outer join exhibits on trades.TRADE_ID = exhibits.EXHIBIT_ID)
+                //③のテーブルにexhibitsテーブルを左外部結合する。
+                  その際、指定条件としてtradesテーブルのTRADE_IDとexhibitsテーブルのEXHIBIT_IDが一致する物を指定する。
+                ⑤left outer join gacha_titles on ticket_types.GACHA_TITLE_ID = gacha_titles.GACHA_TITLE_ID)
+                //④のテーブルにgacha_titlesテーブルを左外部結合する。
+                  その際、指定条件としてticket_typesテーブルのGACHA_TITLE_IDとgacha_titlesテーブルのGACHA_TITLE_IDが一致する物を指定する。
+                  ticketsテーブルを基準としてticket_types,ticket_trades,trades,exhibits,gacha_titlesテーブルを結合し、一致するものを統合しながら一つのテーブルとする。
+              where tickets.USER_ID = ? and gacha_titles.GACHA_TITLE_ID = ?";
+              //ticketsテーブルのUSER_IDとgacha_titlesテーブルのGACHA_TITLE_IDが一致するものを指定
+          //実行する
+          $stmt = $this->query($sql, [$user_id,$value]);
+          //結果セットを取り出す
+          $records = $stmt->fetchAll( );
+          //結果セットを戻り値として返す
+          return  $records;
+       }
+     }
+?>

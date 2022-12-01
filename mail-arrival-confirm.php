7a9941772ce19fe56aec7a558fa8292aee285da6
nakagoe <kd1307490@st.kobedenshi.ac.jp>
2022-12-01 14:13:32 +0900

    到着確認画面

---
+++ mail-arrival-confirm.php
@@ -0,0 +1,43 @@
+<!-- ヘッドの全体に関わる共有部分 -->
+<?php require_once('./temp/head.php'); ?>
+<!-- /ヘッドの全体に関わる共有部分 -->
+
+<!-- ↓↓↓　ここに各画面専用のスタイルのリンクタグを書きます ↓↓↓ -->
+
+<link rel="stylesheet" href="./css/mailconfirm.css">
+
+<!-- ↑↑↑　/ここに各画面専用のスタイルのリンクタグを書きます　↑↑↑ -->
+
+<!-- ヘッダー -->
+<?php require_once("./temp/header.php"); ?>
+<!-- /ヘッダー -->
+
+    <main class="main-side-content">
+      <section class="main-content">
+        <!-- mainコンテンツ -->
+        
+
+        <!-- 到着が未完了と完了時で表示を切り替える -->
+      <div class="center">
+        <h1 class="sub-ttl ">〇〇(商品名)は郵送中です。</h1>
+        <h1 class="none sub-ttl">〇〇(商品名)は郵送完了しました。</h1>
+      </div>
+       <!-- /到着が未完了と完了時で表示を切り替える -->
+       <div class="btn-block">
+        <a class="back-btn" href="./">戻る</a>
+        <!-- <a class="confirm-btn" href="./transaction-information.php">トレード詳細画面へ</a> -->
+       </div>
+
+
+      </section>
+
+<!-- サイドコンテンツ -->
+<?php require_once('./temp/side.php'); ?>
+<!-- /サイドコンテンツ -->
+
+</main>
+<!-- /mainコンテンツ -->
+
+<!-- フッター -->
+<?php require_once('./temp/footer.php'); ?>
+<!-- /フッター -->

a9c958d252224a351646318d1c34a13402d62cc2
Naoto000 <kd1289594@st.kobedenshi.ac.jp>
2022-12-05 11:36:28 +0900

    リファクタリング

--- chat_db.php
+++ chat_db.php
@@ -1,48 +1,32 @@
 <?php
 
-    // チャット内容
-    $chat_text = $_POST["chat_text"];
-    // チャット相手のユーザーID
-    $partner_user_id = $_GET['id'];
+    require_once __DIR__ . './classes/chat_db_class.php';
 
     // チャットの入力欄が空欄で送信マークが押された時、何もせずに戻る
-    if(empty($chat_text) == True){
-        header('Location:chat.php?id='.$partner_user_id.'');
+    if(empty($_POST["chat_text"]) == True){
+        header('Location:chat.php?id='.$_GET['id'].'');
     // チャットの入力欄に何かが入力された状態で送信ボタンが押された時、DBに内容を保存
     } else {
         session_start();    // セッションのスタート
 
-        date_default_timezone_set ('Asia/Tokyo');   // 日本時間に設定
-        $chat_time = date('Y-m-d H:i:s');   // チャット時間の取得
+        // チャット時刻の取得
+        date_default_timezone_set ('Asia/Tokyo');
+        $chat_time = date('Y-m-d H:i:s');
 
-        $user_id = $_SESSION['user_id'];    // ユーザーID
-
-        
-        require_once __DIR__ . './classes/dbdata.php';
-        $exh = new Dbdata();
-        $sql = "select TRADE_ID from CHATS where USER_ID = '" . $_SESSION['user_id'] . "' and PARTNER_USER_ID = '" . $partner_user_id . "'";
-        $data = $exh->getRecord_0($sql);
+        // トレードIDの取得
+        $trade_id = new Chat_db_trade_id();
+        $data = $trade_id->get_trade_id($_SESSION['user_id'],$_GET['id']);
 
         foreach($data as $info){
             $trade_id = $info['TRADE_ID'];
         }
 
+        // チャット内容をDBに
+        $insert = new Chat_db_insert();
+        $data_insert = $insert->insert($trade_id,$_SESSION['user_id'],$_GET['id'],$chat_time,$_POST["chat_text"]);
 
-        // データ格納（flag無し）
-        $pdo = new PDO(
-            'mysql:host=localhost;dbname=torepon;charset=utf8',     //　mysql:host=localhost;dbname="作成したデータベース名”;charset=utf8
-            'shopping',     // ユーザー名
-            'site');        // パスワード 
-        $pdo->query('SET NAMES utf8;');
-        $stmt = $pdo->prepare('INSERT INTO chats(TRADE_ID,USER_ID,PARTNER_USER_ID,CHAT_TIME,CHAT_TEXT) VALUES(:trade_id,:user_id,:partner_user_id,:chat_time,:chat_text)');
-        $stmt->bindValue(':trade_id', $trade_id);
-        $stmt->bindValue(':user_id', $user_id);
-        $stmt->bindValue(':partner_user_id', $partner_user_id);
-        $stmt->bindValue(':chat_time', $chat_time);
-        $stmt->bindValue(':chat_text', $chat_text);
-        $stmt->execute();
-        unset($pdo);
-        header('Location:chat.php?id='.$partner_user_id.'');
     }
+
+    header('Location:chat.php?id='.$_GET['id'].'');
 
 ?>

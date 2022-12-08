d81694734fb4868f186a3e552e174f6358b85a73
nakagoe <kd1307490@st.kobedenshi.ac.jp>
2022-12-08 19:22:09 +0900

    chat画面修正

--- chat2.php
+++ chat2.php
@@ -121,8 +121,8 @@
         </ul>
     
         <div class="my-message">
-        <a href = "tr_forward.php?id=<?php echo $trade_id ?>">トレードする</a>
-          <a href = "cancel.html">トレードしない</a>
+          <!-- <a href = "cancel.html">取引キャンセル申請</a>
+          <a href = "tr_forward.php?id=<?php //echo $trade_id ?>">取引進行申請</a> -->
           <div>
             <form action="./chat_db.php?id=<?php echo $_SESSION['id'] ?>" method="post" name="chat_form">
               <input class="message-send" type="text" placeholder="メッセージを入力" name="chat_text">
